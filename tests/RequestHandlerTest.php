77022bc1658e3c6f023cbbfe6b9afc16e836f5b9
Alessandro Chitolina <alekitto@gmail.com>
2021-02-11 17:33:27 +0100

    add more tests for request handler

--- tests/RequestHandlerTest.php
+++ tests/RequestHandlerTest.php
@@ -58,6 +58,17 @@ class RequestHandlerTest extends TestCase
         self::assertFalse($response->headers->has('Access-Control-Allow-Origin'));
     }
 
+    public function testShouldAddStarOriginIfNoOriginIsSpecified(): void
+    {
+        $request = Request::create('/');
+        $request->headers->set('Origin', 'http://example.org');
+        $response = new Response();
+
+        $this->handler = new RequestHandler(true);
+        $this->handler->enhanceResponse($request, $response);
+        self::assertEquals('*', $response->headers->get('Access-Control-Allow-Origin'));
+    }
+
     public function testShouldNotAddHeadersIfOriginIsNotValid(): void
     {
         $request = Request::create('/');
@@ -66,6 +77,34 @@ class RequestHandlerTest extends TestCase
         $response = new Response();
         $this->handler->enhanceResponse($request, $response);
         self::assertFalse($response->headers->has('Access-Control-Allow-Origin'));
+    }
+
+    public function testShouldAddAllowedHeaders(): void
+    {
+        $request = Request::create('/');
+        $request->headers->set('Origin', 'http://example.org');
+        $request->headers->set('Access-Control-Request-Headers', 'X-Custom-Header, Range, X-Unknown');
+        $response = new Response();
+
+        $this->handler = new RequestHandler(true, ['*'], ['X-Custom-Header', 'Range', 'Accept']);
+        $this->handler->enhanceResponse($request, $response);
+
+        self::assertEquals('X-Custom-Header,Range', $response->headers->get('Access-Control-Allow-Headers'));
+    }
+
+    public function testShouldMatchGlobOrigin(): void
+    {
+        $request = Request::create('/');
+        $request->headers->set('Origin', 'http://example.org');
+        $response = new Response();
+
+        $this->handler = new RequestHandler(true, ['http://localhost', 'http://example.*']);
+        $this->handler->enhanceResponse($request, $response);
+        self::assertEquals('http://example.org', $response->headers->get('Access-Control-Allow-Origin'));
+
+        $this->handler = new RequestHandler(true, ['http://example.?rg', 'xx\*?']);
+        $this->handler->enhanceResponse($request, $response);
+        self::assertEquals('http://example.org', $response->headers->get('Access-Control-Allow-Origin'));
     }
 
     public function testShouldSetTheCorrectAllowControlOriginOnResponse(): void
